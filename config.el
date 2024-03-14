9831562119f67e9c2c222797a186f22bfd6c37a8
mikkelwillen <mikkel@willen.dk>
2024-03-14 21:14:37 +0100

    fontsize and pdf-tools

--- config.el
+++ config.el
@@ -45,6 +45,9 @@
 ;; Change the undo limit to 80MB
 (setq undo-limit 80000000)
 
+;; Set the font size
+(setq doom-font (font-spec :size 16))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
